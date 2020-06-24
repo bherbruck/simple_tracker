0fc3db086ed324f3d5a00cc48be1f1ebd922f6f1
bherbruck <brennen.herbruck@gmail.com>
2020-06-24 16:02:11 -0400

    add tests and test data

--- tests/test_tracker.py
+++ tests/test_tracker.py
@@ -1,3 +1,6 @@
+import json
+import pytest
+
 from simple_tracker import Tracker
 
 
@@ -16,10 +19,34 @@ def test_update():
         0: (10, 5, 5, 1), 1: (50, 0, 0, 0), 2: (50, 10, 0, 1)}
     assert tracker.update([(10, 10), (50, 10)]) == [
         {'id': 1, 'x': 50, 'y': 0, 'distance': 0, 'last_frame': 0}]
-    print(tracker.points)
     assert tracker.points == {0: (10, 10, 10.0, 2), 2: (50, 10, 0.0, 2)}
     tracker.update([])
 
 
-def test_update_cb():
-    pass
+
+def test_real_data():
+    with open('tests/data/data.json', 'r') as file:
+        data = json.load(file)
+    tracker = Tracker(max_distance=data['tracker']['max_distance'],
+                      timeout=data['tracker']['timeout'])
+    tracker.frame = data['data'][0]['frame']
+    
+    def tracking_found_cb(point):
+        return (
+            point[1] < 140 or
+            tracker.frame == 0
+        )
+
+    def tracking_lost_cb(point):
+        return (
+            point['y'] >= 140 and
+            point['distance'] >= 30
+        )
+
+    for frame in data['data']:
+        tracker.update(frame['test_data'], tracking_found_cb=tracking_found_cb,
+                       tracking_lost_cb=tracking_lost_cb)
+        
+        points = [int(k) for k in frame['assertations'].keys()]
+        assert sorted(tracker.points.keys()) == sorted(points)
+    
