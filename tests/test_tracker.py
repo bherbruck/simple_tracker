1046930f6fbcb41849365d7d45464e7be7cea87a
bherbruck <brennen.herbruck@gmail.com>
2020-06-24 11:40:43 -0400

    Update test_tracker.py

--- tests/test_tracker.py
+++ tests/test_tracker.py
@@ -1,5 +1,25 @@
 from simple_tracker import Tracker
 
 
-def test_Tracker():
-    tracker = Tracker()
+test_data = [
+    [(10, 0), (50, 0)],
+    [(10, 5), (50, 10)]
+]
+
+
+def test_update():
+    tracker = Tracker(max_distance=5, timeout=2)
+    tracker.update(test_data[0])
+    assert tracker.points == {0: (10, 0, 0, 0), 1: (50, 0, 0, 0)}
+    tracker.update(test_data[1])
+    assert tracker.points == {
+        0: (10, 5, 5, 1), 1: (50, 0, 0, 0), 2: (50, 10, 0, 1)}
+    assert tracker.update([(10, 10), (50, 10)]) == [
+        {'id': 1, 'x': 50, 'y': 0, 'distance': 0, 'last_frame': 0}]
+    print(tracker.points)
+    assert tracker.points == {0: (10, 10, 10.0, 2), 2: (50, 10, 0.0, 2)}
+    tracker.update([])
+
+
+def test_update_cb():
+    pass
