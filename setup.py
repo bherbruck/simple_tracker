b80156a1e659aa07890942ba146d5936f082c78f
bherbruck <brennen.herbruck@gmail.com>
2020-06-28 22:51:52 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='simple_tracker',
-    version='0.1.1',
+    version='0.1.2',
     author='Brennen Herbruck',
     author_email='brennen.hrbruck@gmail.com',
     description='Track multiple objects using just x, y coordinates',
