a9143b56ef52a208e3786c86e0f153b2cc3ea1ed
bherbruck <brennen.herbruck@gmail.com>
2020-06-30 15:25:56 -0400

    added logging for creating test data

--- examples/openmv/simple_tracking.py
+++ examples/openmv/simple_tracking.py
@@ -1,6 +1,7 @@
 import sensor
 import image
 import time
+import json
 
 from simple_tracker import Tracker
 
@@ -8,14 +9,24 @@ from simple_tracker import Tracker
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.QVGA)
+sensor.skip_frames(time=2000)
+sensor.set_auto_gain(False)
+sensor.set_auto_whitebal(False)
 clock = time.clock()
 
-orange_threshold = [(23, 79, 19, 54, 4, 57)]
-green_threshold = [(10, 100, -67, -23, 2, 54)]
+green_threshold = [(44, 88, -50, 18, 9, 66)]
 tracker = Tracker(max_distance=50)
-
+record = True
+tracker_file = 'tracker.txt'
+data_file = 'data.txt'
 
 def main():
+    if record:
+        data = {'tracker': {'max_distance': tracker.max_distance,
+                            'timeout': tracker.timeout}}
+        write_file(tracker_file, data)
+        write_file(data_file, '')
+
     while(True):
         clock.tick()
         img = sensor.snapshot()
@@ -24,12 +35,32 @@ def main():
 
 
 def find_blobs(img):
-    blobs = img.find_blobs(green_threshold)
-    tracker.update([(blob.cx(), blob.cy()) for blob in blobs])
+    blobs = img.find_blobs(green_threshold, pixels_threshold=500)
+    centroids = [(blob.cx(), blob.cy()) for blob in blobs]
+    lost_tracking = tracker.update(centroids)
     for blob in blobs:
         img.draw_rectangle(blob.rect())
     for id, point in tracker.points.items():
         img.draw_string(point[0], point[1], str(id), scale=2)
+
+    if record:
+        # ujson isn't the best... so record like this
+        data = {
+            'frame': tracker.frame,
+            'test_data': centroids,
+            'assertations': tracker.points,
+            'counts': lost_tracking
+        }
+        append_file(data_file, data)
+
+
+def write_file(path, data):
+    with open(path, 'w') as file:
+        file.write(str(data))
+
+def append_file(path, data):
+    with open(path, 'a') as file:
+        file.write(str(data) + ',\n')
 
 
 main()
